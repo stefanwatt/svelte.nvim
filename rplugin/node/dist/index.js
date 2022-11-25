7f414de3fb89ac958ac82ce10af2123a529ad041
Stefan Watt <stefan.watt@gmail.com>
2022-11-25 18:43:03 +0100

    initial commit

--- rplugin/node/dist/index.js
+++ rplugin/node/dist/index.js
@@ -1,9 +1,7 @@
 "use strict";
-const exportComponent_1 = require("./exportComponent");
-const nvim_1 = require("./nvim");
 module.exports = (plugin) => {
-    const nvim = new nvim_1.Nvim(plugin.nvim);
-    plugin.registerCommand('SvelteExportComponent', exportComponent_1.exportComponent, { sync: false });
+    // const nvim = new Nvim(plugin.nvim)
+    // plugin.registerCommand('SvelteExportComponent', exportComponent, { sync: false })
 };
 // const main = async () => {
 //   const parser = new Parser()
