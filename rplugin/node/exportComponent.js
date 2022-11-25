262ad0f30efe2ca9b87e52bd2fb71f78ce04f22f
Stefan Watt <stefan.watt@gmail.com>
2022-11-25 18:22:07 +0100

    initial commit

---
+++ rplugin/node/exportComponent.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.exportComponent = void 0;
+const exportComponent = (nvim) => () => {
+    nvim.print("exporting svelte component...");
+};
+exports.exportComponent = exportComponent;
+//# sourceMappingURL=exportComponent.js.map
