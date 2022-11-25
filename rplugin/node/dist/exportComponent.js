6bca060e4c45f07a66a6061e6e238485631a7b80
Stefan Watt <stefan.watt@gmail.com>
2022-11-25 19:06:21 +0100

    test remove treesitter

--- rplugin/node/dist/exportComponent.js
+++ rplugin/node/dist/exportComponent.js
@@ -1,19 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.exportComponent = void 0;
-const Parser = require("tree-sitter");
-const Svelte = require("tree-sitter-svelte");
 const exportComponent = (nvim) => async () => {
     nvim.print("exporting svelte component...");
-    const parser = new Parser();
-    parser.setLanguage(Svelte);
-    const sourceCode = await nvim.getBufText();
-    const tree = parser.parse(sourceCode);
-    // const query = new Query(Svelte, sourceCode)
-    // console.log(query.predicates)
-    // const matches = query.matches(tree.rootNode)
-    const expressions = tree.rootNode.descendantsOfType('expression');
-    const reference = expressions[0].descendantsOfType('raw_text_expr')[0];
-    nvim.print(tree.rootNode.children[3].children[0].children[3].toString());
+    // const parser = new Parser()
+    // parser.setLanguage(Svelte)
+    // const sourceCode = await nvim.getBufText()
+    // const tree = parser.parse(sourceCode)
+    // // const query = new Query(Svelte, sourceCode)
+    // // console.log(query.predicates)
+    // // const matches = query.matches(tree.rootNode)
+    // const expressions = tree.rootNode.descendantsOfType('expression')
+    // const reference = expressions[0].descendantsOfType('raw_text_expr')[0]
+    // nvim.print(tree.rootNode.children[3].children[0].children[3].toString())
 };
 exports.exportComponent = exportComponent;
--- rplugin/node/src/exportComponent.ts
+++ rplugin/node/src/exportComponent.ts
@@ -4,15 +4,15 @@ import * as Svelte from 'tree-sitter-svelte'
 
 export const exportComponent = (nvim: Nvim)=> async ()=>{
   nvim.print("exporting svelte component...")
-  const parser = new Parser()
-  parser.setLanguage(Svelte)
+  // const parser = new Parser()
+  // parser.setLanguage(Svelte)
 
-  const sourceCode = await nvim.getBufText()
-  const tree = parser.parse(sourceCode)
-  // const query = new Query(Svelte, sourceCode)
-  // console.log(query.predicates)
-  // const matches = query.matches(tree.rootNode)
-  const expressions = tree.rootNode.descendantsOfType('expression')
-  const reference = expressions[0].descendantsOfType('raw_text_expr')[0]
-  nvim.print(tree.rootNode.children[3].children[0].children[3].toString())
+  // const sourceCode = await nvim.getBufText()
+  // const tree = parser.parse(sourceCode)
+  // // const query = new Query(Svelte, sourceCode)
+  // // console.log(query.predicates)
+  // // const matches = query.matches(tree.rootNode)
+  // const expressions = tree.rootNode.descendantsOfType('expression')
+  // const reference = expressions[0].descendantsOfType('raw_text_expr')[0]
+  // nvim.print(tree.rootNode.children[3].children[0].children[3].toString())
 }
